2dc2bb51acb50cd630c23138b4668fe8d0bc3142
qinlingfeng0404 <475117300@qq.com>
2017-04-21 15:38:58 +0800

    Initial commit

---
+++ js/index.js
@@ -0,0 +1,11 @@
+/*function text_01(){
+    // var val=0+"px";
+    document.getElementById('rightsidebar').style.right=0;
+}
+function text_02(){
+    // var val=-162+"px";
+    document.getElementById('rightsidebar').style.right=-162+'px';
+}*/
+function text_03(){
+    document.getElementById('rightsidebar_title').style.display='none';
+}
